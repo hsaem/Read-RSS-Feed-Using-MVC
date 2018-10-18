eea2f6a6c2065cd4a9a4bcd7654fa4d406013fa0
hsaem <hsaem92@gmail.com>
2018-10-18 10:48:12 +0330

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,53 @@
+﻿using RssFeedRead.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace RssFeedRead.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string RSSURL)
+        {
+            WebClient wclient = new WebClient();
+            wclient.Encoding = Encoding.UTF8;
+            string RSSData = wclient.DownloadString(RSSURL);
+
+            XDocument xml = XDocument.Parse(RSSData);
+            var RSSFeedData = (from x in xml.Descendants("item")
+                               select new RSSFeed
+                               {
+                                   Title = ((string)x.Element("title")),
+                                   Link = ((string)x.Element("link")),
+                                   Description = ((string)x.Element("description")),
+                                   PubDate = ((string)x.Element("pubDate"))
+                               });
+            ViewBag.RSSFeed = RSSFeedData;
+            ViewBag.URL = RSSURL;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
